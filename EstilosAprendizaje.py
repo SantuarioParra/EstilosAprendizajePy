a7d31ab88ca2e28de278100a3a505380d308bc90
Santuario Parra Luis Fernando <36172678+SantuarioParra@users.noreply.github.com>
2019-11-27 12:33:19 -0600

    Casi listo

--- EstilosAprendizaje.py
+++ EstilosAprendizaje.py
@@ -1,9 +1,11 @@
 import argparse
 import pandas as pn
 import numpy as np
+from matplotlib import pyplot as ptl
+import matplotlib.backends.backend_pdf as mpdf
 
 
-def read_excel(file_url):
+def read_excel(file_url, fileO_url):
     xlp = pn.read_excel(file_url)  # se carga el archivo de excel a un pandas
 
     xlp = xlp.rename(
@@ -11,11 +13,11 @@ def read_excel(file_url):
 
     # se remplazan las respuestas a) y b), los datos del alumno van de la pos 0 a 4
     xlp = xlp.replace(to_replace=r'(^a\).*)',
-                      value=1,
+                      value=-1,
                       regex=True)
 
     xlp = xlp.replace(to_replace=r'(^b\).*)',
-                      value=-1,
+                      value=1,
                       regex=True)
     # se crea el data set de resultados
     data_result = pn.DataFrame({'NAME': xlp.loc[:, 2], 'EMAIL': xlp.loc[:, 1], 'TIMESTAMP': xlp.loc[:, 0],
@@ -37,18 +39,45 @@ def read_excel(file_url):
     data_result['VV'] = xlp.loc[:, [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42]].sum(axis=1)
     data_result['SG'] = xlp.loc[:, [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43]].sum(axis=1)
 
-    #print(xlp.loc[:, [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40]])
+    # print(xlp.loc[:, [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40]])
     with pn.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         print(data_result)
+    headers = list(['Activo', 'Sensorial', 'Visual', 'Secuencial'])
+    headers2 = list(['Reflexivo', 'Intuitivo', 'Verbal', 'Global'])
+    headers_val = range(0, 4)
+    pdf = mpdf.PdfPages(fileO_url)
+    for index in range(0, data_result.shape[0]):
+        fig, ax1 = ptl.subplots()
+        ptl.yticks(range(-11, 12))
+        ptl.ylim(-11, 11)
+        ax1.set_xlabel("Estilo de aprendizaje")
+        ax1.set_ylabel("Nivel de Estilo de aprendizaje")
+        ax1.tick_params(axis='x')
+        ax1.set_xticks(headers_val)
+        ax1.set_xticklabels(headers, fontdict=None, minor=False)
+        ax2 = ax1.twiny()  # instantiate a second axes that shares the same x-axis
+        # we already handled the x-label with ax1
+        ax2.set_xticks(headers_val)
+        ax2.set_xticklabels(headers2, fontdict=None, minor=False)
+        ax2.tick_params(axis='x', top=True)
+        fig.tight_layout()  # otherwise the right y-label is slightly
+        results_val = list(data_result.iloc[index, 5:])
+        ptl.stem(headers_val, results_val)
+        ptl.savefig('grafica.png')
+
+    #pdf.savefig("grafica.png")
+    #pdf.close()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", help="Nombre del archivo")
+    parser.add_argument("-f", "--file", help="Nombre del archivo a tratar")
+    parser.add_argument("-o", "--fileO", help="Nombre del archivo de salida")
     args = parser.parse_args()
-    if args.file:
+    if args.file and args.fileO:
         print("Archivo seleccionado: " + args.file)
-        read_excel(args.file)
+        print("Archivo salida: " + args.fileO)
+        read_excel(args.file, args.fileO)
     else:
         print("No se selecciono ningun archivo")
 
